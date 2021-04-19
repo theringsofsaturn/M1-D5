b67847e433a9cfbdf6c6aed1bc28c5f500796401
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-04-19 02:07:25 +0200

    revising ex. 11

--- homework.js
+++ homework.js
@@ -250,6 +250,10 @@ function isTodayMyBirthday() {
    Write a function called "deleteProp" which receives an object and a string as parameters, and returns the given object after deleting its property named as the given string.
 */
 
+function deleteProp(obj, prop){
+  delete obj[prop]
+  return obj
+
 /* Ex.12 
     Write a function called "olderMovie" which finds the oldest movie in the array provided at the end of this file.
 */
